b9341dc1b05d6a36f018ac5ca8e6caf5edcd7ad0
Dwi Janto <dwijanto10@gmail.com>
2023-03-20 09:04:28 +0700

    first commit

---
+++ app/src/main/java/com/example/apidasar/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.apidasar;
+
+import android.app.ProgressDialog;
+import android.graphics.Movie;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import com.example.apidasar.databinding.ActivityMainBinding;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.rvMovie.setLayoutManager(new GridLayoutManager(this,2));
+
+        getPopularMovie();
+    }
+
+    private void getPopularMovie(){
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        //TODO
+        //Get Data From API
+    }
+}
