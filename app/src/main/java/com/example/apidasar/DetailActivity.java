d3c7a7fca17e8b5f1f9bf36066446d552b45de87
Dwi Janto <dwijanto10@gmail.com>
2023-03-27 09:10:56 +0700

    first commit

--- app/src/main/java/com/example/apidasar/DetailActivity.java
+++ app/src/main/java/com/example/apidasar/DetailActivity.java
@@ -2,16 +2,24 @@ package com.example.apidasar;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Movie;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.apidasar.databinding.ActivityDetailBinding;
+import com.squareup.picasso.Picasso;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class DetailActivity extends AppCompatActivity {
     private ActivityDetailBinding binding;
     private ProgressDialog progressDialog;
@@ -35,11 +43,56 @@ public class DetailActivity extends AppCompatActivity {
         progressDialog.setMessage("Loading....");
         progressDialog.show();
 
+        /*Create handle for the RetrofitInstance interface*/
+        ApiService service = ApiClient.getRetrofitInstance().create(ApiService.class);
+        String apiKey = getResources().getString(R.string.api_key);
+        Call<Movie> call = service.getDetailMovie(movieId,apiKey);
+        call.enqueue(new Callback<Movie>() {
+            @Override
+            public void onResponse(Call<Movie> call, Response<Movie> response) {
+                progressDialog.dismiss();
+                Movie movie = response.body();
+                setDataUI(movie);
+            }
 
+            @Override
+            public void onFailure(Call<Movie> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(DetailActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    private void setDataUI() {
+    private void setDataUI(Movie movie) {
+        binding.movieName.setText(movie.getTitle());
+        binding.movieRating.setText(String.valueOf(movie.getVoteAverageRound()));
+        binding.movieDescription.setText(movie.getOverview());
+        binding.movieGenre.setText(movie.getGenres());
+        binding.movieRatingCount.setText(getString(R.string.rating_count, movie.getVoteCount()));
 
+        String releaseDate = movie.getReleaseDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyy-mm-dd");
+        Date date;
+        try {
+            date = simpleDateFormat.parse(releaseDate);
+            simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
+            releaseDate = simpleDateFormat.format(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        binding.movieReleaseDate.setText(getString(R.string.release, releaseDate));
+
+        String baseUrlImage = getString(R.string.base_url_image_w500);
+        String urlPoster = baseUrlImage + movie.getBackdropPath();
+        Picasso.get()
+                .load(urlPoster)
+                .placeholder(R.drawable.image_placeholder)
+                .error(R.drawable.image_placeholder_error)
+                .into(binding.moviePoster);
+
+        setFabFavorite();
+        setBtnShare(getString(R.string.url_imbd, movie.getImdbId()));
+        setBtnBrowser(getString(R.string.url_imbd, movie.getImdbId()));
     }
 
     private void setFabFavorite(){
@@ -59,6 +112,7 @@ public class DetailActivity extends AppCompatActivity {
         binding.btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent2 = new Intent(); intent2.setAction(Intent.ACTION_SEND);
                 intent2.setType("text/plain");
                 intent2.putExtra(Intent.EXTRA_TEXT, value );
--- app/src/main/java/com/example/apidasar/Movie.java
+++ app/src/main/java/com/example/apidasar/Movie.java
@@ -1,38 +1,85 @@
 package com.example.apidasar;
 
+import java.util.List;
 import com.google.gson.annotations.SerializedName;
 
 public class Movie {
 
+	@SerializedName("backdrop_path")
+	private String backdropPath;
+
+	@SerializedName("overview")
+	private String overview;
+
 	@SerializedName("release_date")
 	private String releaseDate;
 
+	@SerializedName("genres")
+	private List<Genre> genres;
+
 	@SerializedName("vote_average")
-	private Object voteAverage;
+	private double voteAverage;
 
 	@SerializedName("id")
 	private int id;
 
+	@SerializedName("imdb_id")
+	private String imdbId;
+
 	@SerializedName("title")
 	private String title;
 
+	@SerializedName("vote_count")
+	private int voteCount;
+
 	@SerializedName("poster_path")
 	private String posterPath;
+
+	public String getBackdropPath(){
+		return backdropPath;
+	}
+
+	public String getOverview(){
+		return overview;
+	}
 
 	public String getReleaseDate(){
 		return releaseDate;
 	}
 
-	public Object getVoteAverage(){
+	public String getGenres(){
+		String result;
+		String[] genresArray = new String[genres.size()];
+		for (int i= 0; i<genres.size(); i++){
+			genresArray[i] = genres.get(i).getName();
+		}
+		result = String.join(", ", genresArray);
+		return result;
+	}
+
+	public double getVoteAverage(){
 		return voteAverage;
+	}
+
+	public double getVoteAverageRound(){
+		int scale = (int) Math.pow(10, 1);
+		return (double) Math.round(voteAverage * scale) / scale;
 	}
 
 	public int getId(){
 		return id;
 	}
 
+	public String getImdbId() {
+		return imdbId;
+	}
+
 	public String getTitle(){
 		return title;
+	}
+
+	public int getVoteCount(){
+		return voteCount;
 	}
 
 	public String getPosterPath(){
